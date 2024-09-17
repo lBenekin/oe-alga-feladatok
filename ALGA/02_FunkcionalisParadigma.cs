cfe4fd9f0c63084d2cb8723b5827ae91d0187502
lBenekin <b3n3kin@gmail.com>
2024-09-17 19:48:44 +0200

    Done for week 2

---
+++ ALGA/02_FunkcionalisParadigma.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Paradigmak
+{
+
+    public class FeltetelesFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajthato
+    {
+        public Func<T,bool> BejaroFeltetel { get; set; }
+        public FeltetelesFeladatTarolo(int meret) : base(meret)
+        {
+
+        }
+
+        public void FeltetelesVegrehajtas(Func<T, bool> feltetel)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (feltetel.Invoke(tarolo[i]))
+                {
+                    tarolo[i].Vegrehajtas();
+                }
+            }
+        }
+
+        public override IEnumerator<T> GetEnumerator()
+        {
+            if (BejaroFeltetel != null)
+            {
+                return new FeltetelesFeladatTaroloBejaro<T>(tarolo, n, BejaroFeltetel);
+            }
+            else
+            {
+                return new FeltetelesFeladatTaroloBejaro<T>(tarolo, n, a => true);
+            }
+        }
+
+        public class FeltetelesFeladatTaroloBejaro<T> : IEnumerator<T>
+        {
+            T[] tarolo;
+            int n;
+            Func<T, bool> bejaroFeltetel;
+            int aktualisIndex = -1;
+
+            public FeltetelesFeladatTaroloBejaro(T[] tarolo, int n, Func<T, bool> bejaroFeltetel)
+            {
+                this.tarolo = tarolo;
+                this.n = n;
+                this.bejaroFeltetel = bejaroFeltetel;
+            }
+            public T Current
+            {
+                get
+                {
+
+                    return tarolo[aktualisIndex];
+                }
+            }
+
+            object IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                while (aktualisIndex < n-1)
+                {
+                    aktualisIndex++;
+                    if (bejaroFeltetel(Current))
+                    {
+                        return true;
+                    }
+                }
+                
+                return false;
+            }
+
+            public void Reset()
+            {
+                aktualisIndex = -1;
+            }
+        }
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,7 +4,13 @@ namespace OE.ALGA.Sandbox
     {
         static void Main()
         {
-            
+            List<int> a = new List<int> { 1, 2, 3, 13, 27, 6, 7, 8, 9, 10 };
+            IEnumerator<int> enumerator = a.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+
+            }
         }
     }
 }
