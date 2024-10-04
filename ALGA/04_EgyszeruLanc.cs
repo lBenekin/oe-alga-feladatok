eedf93486b9030ed005ca822fcd378542f03193e
Bence <b3n3kin@gmail.com>
2024-10-04 11:39:37 +0200

    Verem kinda kész

---
+++ ALGA/04_EgyszeruLanc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Adatszerkezetek
+{
+    public class LancoltVerem<T>
+    {
+        public LancElem<T> fej { get; set; }
+        public bool Ures => fej.tart == null;
+
+        public LancoltVerem() 
+        {
+            fej = null;
+        }
+        public void Verembe(T ertek)
+        {
+            if (Ures)
+            {
+                throw new NincsElemKivetel();
+            }
+            LancElem<T> temp = new LancElem<T>(ertek, fej);
+            fej = temp;
+        }
+
+        public T Verembol()
+        {
+            if (Ures)
+            {
+                throw new NincsElemKivetel();
+            }
+            T temp = fej.tart;
+            fej = fej.kov;
+            return temp;
+        }
+        public T Felso()
+        {
+            if (Ures)
+            {
+                throw new NincsElemKivetel();
+            }
+            return fej.tart;
+        }
+    }
+    public class LancElem<T>
+    {
+        public T tart {  get; set; }
+        public LancElem<T> kov { get; set; }
+
+        public LancElem(T tart, LancElem<T> kov)
+        {
+            this.tart = tart;
+            this.kov = kov;
+        }
+    }
+}
