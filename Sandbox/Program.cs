e77932ea60bd34ba5b7ce9b8732ce5777701d3fc
lBenekin <b3n3kin@gmail.com>
2024-12-16 21:09:45 +0100

    A teszt nem ment szóval megcsináltam a week 1-hez

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,10 +4,7 @@ namespace OE.ALGA.Sandbox
     {
         static void Main()
         {
-            DateTime a = DateTime.Parse("2024-09-01T10:00:00Z");
-            DateTime b = DateTime.Parse("2024-09-01");
-            Console.WriteLine(a.Date == b);
-
+            
         }
 
     }
